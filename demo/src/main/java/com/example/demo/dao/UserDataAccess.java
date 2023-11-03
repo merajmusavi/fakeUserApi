3d25b6a1086af58fc4b5c31252175672e7469ddb
merajmusavi <merajmusavi1243m@gmail.com>
2023-11-03 16:17:39 +0330

    database

--- demo/src/main/java/com/example/demo/dao/UserDataAccess.java
+++ demo/src/main/java/com/example/demo/dao/UserDataAccess.java
@@ -1,13 +1,23 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.User;
+import org.flywaydb.core.internal.jdbc.JdbcTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+
 @Repository("postgres")
-public class UserDataAccess implements UserDao{
+public class UserDataAccess implements UserDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public UserDataAccess(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertUser(UUID id, User user) {
         return 0;
@@ -19,18 +29,44 @@ public class UserDataAccess implements UserDao{
     }
 
     @Override
+
+
+    @Override
     public List<User> selectAllUser() {
-        return List.of(new User(UUID.randomUUID(),0,"from db"));
+        final String select = "SELECT id, age, name FROM user";
+
+        List<User> users = jdbcTemplate.query(select, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            int age = resultSet.getInt("age");
+            String name = resultSet.getString("name");
+            return new User(id, age, name);
+        });
+
+        return users;
     }
+
 
     @Override
     public Optional<User> selectUserById(UUID id) {
-        return Optional.empty();
+        String selectUserById = "SELECT id, name, age FROM users WHERE id = ?";
+
+        User user = jdbcTemplate.queryForObject(selectUserById, new Object[] { id }, (resultSet, i) -> {
+            UUID userId = UUID.fromString(resultSet.getString("id"));
+            int age = resultSet.getInt("age");
+            String name = resultSet.getString("name");
+            return new User(userId, age, name);
     }
 
     @Override
     public int deleteUserById(UUID id) {
-        return 0;
+            String deleteSql = "DELETE FROM users WHERE id = ?";
+
+            int rowsAffected = jdbcTemplate.update(deleteSql, id.toString());
+
+            if (rowsAffected == 0) {
+                // Handle the case where no user was deleted (e.g., user not found).
+                // You can throw an exception or perform other appropriate error handling.
+            }
     }
 
     @Override
